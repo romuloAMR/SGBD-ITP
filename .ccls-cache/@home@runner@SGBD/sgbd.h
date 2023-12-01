64e76bd6cd6d4326c331607a75f7d14d9f244e05
ROMULOROCHA6 <24809409-ROMULOROCHA6@users.noreply.replit.com>
2023-12-01 23:40:55 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@SGBD/sgbd.h
@@ -0,0 +1,87 @@
+//--||Includes||--
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+//--|||Functions|||--
+
+//Made for Rômulo
+void serverOn(){
+  FILE *server;
+  server = fopen("struct.txt", "a" );
+  fclose(server);
+}
+
+void makeTable(){
+  //To make
+  /*
+    1-usuário deve informar o nome da tabela;
+    2-os tipos poderão ser os tipos primitivos em C
+    3-os valores deverão ser armazenados em arquivo;
+    4-na criação da tabela deverá ser solicitado um nome da chave primária
+    5-a chave primária deve ser do tipo inteiro sem sinal
+  */
+}
+
+void showTables(){
+  //To make
+  //1-deverá mostrar para o usuário as tabelas existentes
+}
+
+void delTable(){
+  //To make
+  //1-usuário deverá fornecer o nome da tabela a ser apagada
+}
+
+int tableExists(char *name){
+  FILE *table;
+  table = fopen(strcat(name, ".txt"), "r" );
+  if(strcmp(name, "struct.txt") == 0 || table == NULL){ 
+    fclose(table);
+    return 0;
+  }
+  fclose(table);
+  return 1;
+}
+
+//Made for Dagson
+void insertData(){
+  //To make
+  /*
+    1-Usuário deve informar o nome da tabela
+    2-sistema deve solicitar os valores de cada uma das colunas
+    3-sistema deve verificar a chave primária
+    4-Em uma tabela deve existir um e apenas um valor de chave primária. Se o usuário        informar uma chave que já existe, sistema deve emitir uma mensagem de erro e não       deve inserir o registro
+  */
+}
+
+void showData(){
+  //To make
+  /*
+    1-Usuário deve informar qual a tabela para serem listados os dados
+    2-os dados deverão ser obtidos a partir do arquivo que armazena o conteúdo da            tabela;
+  */
+}
+
+void searchData(){
+  //To make
+  /*
+    1-Usuário deverá informar o nome da tabela onde realizará a pesquisa
+    2-Sistema deverá fornecer as colunas disponíveis na tabela o usuário deverá              selecionar uma delas
+    3-Sistema deverá solicitar o valor para pesquisar, disponibilizando algumas opções
+        valores maior que o valor informado
+        valores maior ou igual que o valor informado
+        valores igual o valor informado
+        valores menor que o valor informado
+        valores menor ou igual que o valor informado
+        valores próximo ao valor informado
+            se aplica apenas se a coluna for do tipo string
+
+  */
+}
+
+void delData(){
+  //To make
+  //1-Usuário deve informar o nome da tabela e a chave primária da tupla a ser apagada
+}
---
+++ .ccls-cache/@home@runner@SGBD/sys.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+//Made for Rômulo
